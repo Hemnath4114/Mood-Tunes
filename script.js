4b9cf804eaac109a7599219a25e307a3456f86da
Hemnath <hemnath04112004@gmail.com>
2025-04-08 20:35:37 +0530

    added the song list and designed the tunes items

--- script.js
+++ script.js
@@ -2,32 +2,346 @@ const tunesSection = document.querySelector('.tunes');
 document.querySelector('.romantic').addEventListener('click',()=>{
 tunesSection.innerHTML=`<div class="tunes-container">
             <div class="tunes-item">
-                <div class="thumbnail"><img src="/images/green.jpg" alt="" width="100px"></div>
-                <div class="song-name">dheema</div>
-                <div class="artist">ani</div>
-                <div class="audio"></div>
+                <div class="thumbnail"><img src="${list.romantic.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.romantic.song1.name}</div>
+                <div class="artist">${list.romantic.song1.artist}</div>
+                <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
             </div>
             <div class="tunes-item">
-                <div class="thumbnail"><img src="/images/green.jpg" alt="" width="100px"></div>
-                <div class="song-name">kadhal kanave</div>
-                <div class="artist">aldon</div>
-                <div class="audio"></div>
+                <div class="thumbnail"><img src="${list.romantic.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.romantic.song2.name}</div>
+                <div class="artist">${list.romantic.song2.artist}</div>
+                 <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
             </div>
             <div class="tunes-item">
-                <div class="thumbnail"><img src="/images/green.jpg" alt="" width="100px"></div>
-                <div class="song-name">wanna be yours</div>
-                <div class="artist">artic monkey</div>
-                <div class="audio"></div>
+                <div class="thumbnail"><img src="${list.romantic.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.romantic.song3.name}</div>
+                <div class="artist">${list.romantic.song3.artist}</div>
+                <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
             </div>
             <div class="tunes-item">
-                <div class="thumbnail"><img src="/images/green.jpg" alt="" width="100px"></div>
-                <div class="song-name">nilave vaa</div>
-                <div class="artist">raja</div>
-                <div class="audio"></div>
+                <div class="thumbnail"><img src="${list.romantic.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.romantic.song4.name}</div>
+                <div class="artist">${list.romantic.song4.artist}</div>
+                 <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
             </div>
         </div>`;
 });
 
 document.querySelector('.energetic').addEventListener('click',()=>{
-    tunesSection.innerHTML=``;
+    tunesSection.innerHTML=`<div class="tunes-container">
+            <div class="tunes-item">
+                <div class="thumbnail"><img src="${list.energetic.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.energetic.song1.name}</div>
+                <div class="artist">${list.energetic.song1.artist}</div>
+                <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
+            </div>
+            <div class="tunes-item">
+                <div class="thumbnail"><img src="${list.energetic.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.energetic.song2.name}</div>
+                <div class="artist">${list.energetic.song2.artist}</div>
+                 <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
+            </div>
+            <div class="tunes-item">
+                <div class="thumbnail"><img src="${list.energetic.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.energetic.song3.name}</div>
+                <div class="artist">${list.energetic.song3.artist}</div>
+                <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
+            </div>
+            <div class="tunes-item">
+                <div class="thumbnail"><img src="${list.energetic.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.energetic.song4.name}</div>
+                <div class="artist">${list.energetic.song4.artist}</div>
+                 <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
+            </div>
+        </div>`;
 })
+
+document.querySelector('.chill').addEventListener('click',()=>{
+    tunesSection.innerHTML=`<div class="tunes-container">
+            <div class="tunes-item">
+                <div class="thumbnail"><img src="${list.chill.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.chill.song1.name}</div>
+                <div class="artist">${list.chill.song1.artist}</div>
+                <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
+            </div>
+            <div class="tunes-item">
+                <div class="thumbnail"><img src="${list.chill.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.chill.song2.name}</div>
+                <div class="artist">${list.chill.song2.artist}</div>
+                 <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
+            </div>
+            <div class="tunes-item">
+                <div class="thumbnail"><img src="${list.chill.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.chill.song3.name}</div>
+                <div class="artist">${list.chill.song3.artist}</div>
+                <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
+            </div>
+            <div class="tunes-item">
+                <div class="thumbnail"><img src="${list.chill.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.chill.song4.name}</div>
+                <div class="artist">${list.chill.song4.artist}</div>
+                 <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
+            </div>
+        </div>`;
+});
+
+document.querySelector('.depression').addEventListener('click',()=>{
+    tunesSection.innerHTML=`<div class="tunes-container">
+            <div class="tunes-item">
+                <div class="thumbnail"><img src="${list.depression.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.depression.song1.name}</div>
+                <div class="artist">${list.depression.song1.artist}</div>
+                <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
+            </div>
+            <div class="tunes-item">
+                <div class="thumbnail"><img src="${list.depression.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.depression.song2.name}</div>
+                <div class="artist">${list.depression.song2.artist}</div>
+                 <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
+            </div>
+            <div class="tunes-item">
+                <div class="thumbnail"><img src="${list.depression.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.depression.song3.name}</div>
+                <div class="artist">${list.depression.song3.artist}</div>
+                <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
+            </div>
+            <div class="tunes-item">
+                <div class="thumbnail"><img src="${list.depression.song1.img}" alt="" width="100px"></div>
+                <div class="song-name">${list.depression.song4.name}</div>
+                <div class="artist">${list.depression.song4.artist}</div>
+                 <div class="audio-player">
+                    <audio id="myAudio" src="your-audio.mp3"></audio>
+                    <i class="fa fa-play play-button" aria-hidden="true"></i>
+                    <div class="progress-container">
+                        <div class="progress-bar"></div>
+                    </div>
+                    <i class="fa fa-volume-up volume-on" aria-hidden="true"></i>
+                </div>
+                <div class="spotify-link"><a href="#">Spotify</a></div>
+            </div>
+        </div>`;
+});
+
+
+const list = {
+    romantic:{
+        song1:{
+            img: "/images/green.jpg",
+            name: "Dheema",
+            artist: "Anirudh",
+        },
+        song2:{
+            img: "/images/green.jpg",
+            name: "Kadhal Kanave",
+            artist: "Aldon",
+        },
+        song3:{
+            img: "/images/green.jpg",
+            name: "Wanna be yours",
+            artist: "Artic Monkey",
+        },
+        song4:{
+            img: "/images/green.jpg",
+            name: "Nilave vaa",
+            artist: "Illayaraja",
+        }
+        
+    },
+
+    energetic:{
+        song1:{
+            img: "/images/green.jpg",
+            name: "Veera Soora",
+            artist: "Yuvan",
+        },
+        song2:{
+            img: "/images/green.jpg",
+            name: "Rayapuram Peteru",
+            artist: "Anirudh",
+        },
+        song3:{
+            img: "/images/green.jpg",
+            name: "What a karuvadu",
+            artist: "Anirudh",
+        },
+        song4:{
+            img: "/images/green.jpg",
+            name: "Pumped up kicks",
+            artist: "Foster the People",
+        }
+        
+    },
+
+    chill:{
+        song1:{
+            img: "/images/green.jpg",
+            name: "Blue",
+            artist: "Yung kai" ,
+        },
+        song2:{
+            img: "/images/green.jpg",
+            name: "Beanie",
+            artist: "Chezile",
+        },
+        song3:{
+            img: "/images/green.jpg",
+            name: "Kathalikkum",
+            artist: "Malathy Laxman",
+        },
+        song4:{
+            img: "/images/green.jpg",
+            name: "I Ain't worried",
+            artist: "OneRepublic",
+        }
+        
+    },
+
+    depression:{
+        song1:{
+            img: "/images/green.jpg",
+            name: "Heavenly",
+            artist: "Cigarettes After Sex",
+        },
+        song2:{
+            img: "/images/green.jpg",
+            name: "Dead Man",
+            artist: "David Kushner",
+        },
+        song3:{
+            img: "/images/green.jpg",
+            name: "Cry",
+            artist: "Cigarettes After Sex",
+        },
+        song4:{
+            img: "/images/green.jpg",
+            name: "Apocalypse",
+            artist: "Cigarettes After Sex",
+        }
+        
+    },
+    
+    
+
+
+}
